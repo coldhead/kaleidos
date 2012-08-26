a47bbaf5b7f558e4159ec3d8a605198065ee0e60
Cold Head <coldheadhead@gmail.com>
2012-08-26 16:19:32 +1000

    Initial import

---
+++ README.md
@@ -0,0 +1,39 @@
+# Kaleidos: CSS3 Kaleidoscope Effects
+
+A simple kaleidoscope effect produced with a bunch of SASS-powered
+CSS3 transforms.
+
+## Parameters
+You can pass parameters via the query string: e.g.
+?n=13&src=http://someurl/someimage.png
+
+* **n** is a positive integer representing the number of "segments" to
+create within the kaleidoscope. The provided CSS enthusiastically
+supports up to 32 segments but the effect works best with lower
+values.
+
+* **src** is a URL for an alternate image.
+
+* Assigning a truthy value to **clean** inhibits the display of the
+link to Github.
+
+## Dedication
+Dedicated to [@jeanettehayes](http://twitter.com/jeanettehayes), who I
+thought might like this kind of thing.
+
+## Minutiae
+The name "kaleidos" is derived from the Ancient Greek καλός (kalos),
+"beautiful, beauty" and εἶδος (eidos), "that which is seen: form,
+shape".
+
+This project is optimised for butterfly wings. A modern browser is
+mandatory. Happiness is mandatory.
+
+This might be more accurately described as a
+[teleidoscope](http://en.wikipedia.org/wiki/Teleidoscope) but who has
+ever heard of such a thing, really.
+
+## TODO
+* Some kind of build process like all the cool kids are doing.
+* Hands-free animation for mobile devices and lazy people.
+* Whatever @miketaylr requires for Opera.
---
+++ js/kaleidoscope.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+
+    // Crude $_GET style array of query string parameters.
+    var parameters = (function (src) {
+	var params = src.split(/&/);
+	$.each(params, function (i, p) {
+	    p = p.replace(/\/$/, '').split(/=/);
+	    var k = p[0].replace(/^\?/, '');
+	    params[k] = p[1] || true;
+	});
+
+	return params;
+    })(location.search);
+
+
+    // PARAMETER: *n* is the number of segements.
+    var n = ~~parameters['n'] || 7;
+    if (n) {
+	var tiles = '';
+	for (var i = 0; i <= n * 2; i++) {
+	    tiles += '<div class="tile t' + i + '"><div class="image"></div></div>';
+	}
+    }
+
+    var $kaleidescope = $('.kaleidoscope');
+    $kaleidescope.addClass('n' + n);
+    $kaleidescope.append(tiles);
+    var $image = $kaleidescope.find('.image');
+
+    // PARAMETER: *src* is the URL for an alternate image.
+    var src = parameters['src'];
+    if (src)
+	$image.css('background-image', 'url(' + src + ')');
+
+    // PARAMETER: *clean* hides the Github link.
+    var clean = parameters['clean'];
+    if (clean)
+	$('#fork').hide();
+
+    // PARAMETER (undocumented): *mode* changes the animation style.
+    var mode = ~~parameters['mode'] || 2;
+
+    var x = 0;
+    var y = 0;
+
+
+    // Project changes in cursor (x, y) onto the image background position.
+   $kaleidescope.mousemove(function (e) {
+       x++;
+       y++;
+
+       var nx = e.pageX, ny = e.pageY;
+       switch (mode) {
+	   case 1: {
+	       nx = -x;
+	       ny = e.pageY;
+	       break;
+	   }
+	   case 2: {
+	       nx = e.pageX;
+	       ny = -y;
+	       break;
+	   }
+	   case 3: {
+	       nx = x;
+	       ny = e.pageY;
+	       break;
+	   }
+	   case 4: {
+	       nx = e.pageX;
+	       ny = y;
+	       break;
+	   }
+	   case 5: {
+	       nx = x;
+	       ny = y;
+	   }
+       }
+
+       $image.css('background-position', [nx + "px", ny + "px"].join(' '));
+   });
+
+});
